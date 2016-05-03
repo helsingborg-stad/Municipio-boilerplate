aec29f40652937c17e172c54c011d5cc5fd66ac9
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-03 10:01:34 +0200

    Added composer and wp-config files

---
+++ config/database-example.php
@@ -0,0 +1,29 @@
+<?php
+
+// ** MySQL settings - You can get this info from your web host ** //
+
+/** The name of the database for WordPress */
+define('DB_NAME', '');
+
+/** MySQL database username */
+define('DB_USER', '');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', '');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
---
+++ config/developer.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+
+// Activate debug mode
+define('WP_DEBUG', true);
+
+// Disabled use of memcached
+define('WP_USE_MEMCACHED', false);
