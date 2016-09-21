e0db89be22d4e84c4aa69619728287590f5b3c5c
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-09-21 16:44:01 +0200

    Added composer url fixer and acf import cleaner.

---
+++ wp-content/mu-plugins/composerUrlFixer.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+Plugin Name:    Composer WordPress Network URL
+Description:    Fixes issues with network-admin & home url when using wp-composer setup.
+Version:        1.0
+Author:         Sebastian Thulin
+*/
+
+namespace ComposerNetworkAdmin;
+
+class composerNetworkAdmin
+{
+    public function __construct()
+    {
+        if (is_multisite() && defined(SUBDOMAIN_INSTALL) && SUBDOMAIN_INSTALL) {
+            add_filter('network_admin_url', array($this, 'sanitizeNetworkAdminUrl'), 50, 2);
+            add_filter('admin_url', array($this, 'sanitizeAdminUrl'), 50, 3);
+        }
+
+        //Remove /wp from home_url.
+        add_filter('option_home', function ($url) {
+            return preg_replace('/\/wp$/', '', $url);
+        });
+    }
+
+    public function sanitizeAdminUrl($url, $path, $blog_id)
+    {
+        if (strpos($url, '/wp/wp-admin') === false && !strpos($url, '/network')) {
+            return str_replace('/wp-admin/', '/wp/wp-admin/', $url);
+        } else {
+            return $url;
+        }
+    }
+
+    public function sanitizeNetworkAdminUrl($url, $path)
+    {
+        if (strpos($url, '/wp/wp-admin/network') === false && strpos($url, '/network')) {
+            return str_replace('/wp-admin/', '/wp/wp-admin/', $url);
+        } else {
+            return $url;
+        }
+    }
+}
+
+new \ComposerNetworkAdmin\ComposerNetworkAdmin();
